97d3f7225cbf8ec03fc0ca5566963d4ef9b85f67
Lucky <donlakshman007@gmail.com>
2018-08-24 07:49:13 +0530

    index mod

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ echo curl_exec($ch);
 
 }else{
 //found
-
+/*
 $curl = curl_init(); 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.uptimerobot.com/v2/deleteMonitor",CURLOPT_RETURNTRANSFER => true,CURLOPT_ENCODING => "",CURLOPT_MAXREDIRS => 10,CURLOPT_TIMEOUT => 30,CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST => "POST",
@@ -161,7 +161,7 @@ curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/x-www-form-u
 curl_setopt($ch,CURLOPT_URL,"https://api.telegram.org/bot523151186:AAH0_tWneKWeEiDkwUbuxZgpUedAOHMTD_k/sendMessage?chat_id=536224432&text=$uprapi ".$uprid." https%3A%2F%2F".$site.".herokuapp.com%2Findex.php%3Fid=".($_GET['id']+0));
 curl_exec($ch);
 }
-	
+*/	
 	
 }//found
 
